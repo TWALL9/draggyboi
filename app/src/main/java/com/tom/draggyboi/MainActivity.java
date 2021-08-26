77cbce8ad182aca5598adb9fc2707d23d1876283
Tom Wallis <twall9862@gmail.com>
2021-08-25 23:50:19 -0400

    Add scrollable textfield

--- app/src/main/java/com/tom/draggyboi/MainActivity.java
+++ app/src/main/java/com/tom/draggyboi/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.view.MotionEventCompat;
 import android.content.ClipData;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
@@ -15,6 +16,7 @@ import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import static android.view.MotionEvent.INVALID_POINTER_ID;
 
@@ -24,67 +26,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.myimage1).setOnTouchListener(new MyTouchListener());
-        findViewById(R.id.myimage2).setOnTouchListener(new MyTouchListener());
-        findViewById(R.id.myimage3).setOnTouchListener(new MyTouchListener());
-        findViewById(R.id.myimage4).setOnTouchListener(new MyTouchListener());
-        findViewById(R.id.topleft).setOnDragListener(new MyDragListener());
-        findViewById(R.id.topright).setOnDragListener(new MyDragListener());
-        findViewById(R.id.bottomleft).setOnDragListener(new MyDragListener());
-        findViewById(R.id.bottomright).setOnDragListener(new MyDragListener());
+        TextView view = findViewById(R.id.textView2);
+        view.setMovementMethod(new ScrollingMovementMethod());
+//        findViewById(R.id.myimage1).setOnTouchListener(new MyTouchListener());
+//        findViewById(R.id.myimage2).setOnTouchListener(new MyTouchListener());
+//        findViewById(R.id.myimage3).setOnTouchListener(new MyTouchListener());
+//        findViewById(R.id.myimage4).setOnTouchListener(new MyTouchListener());
+//        findViewById(R.id.topleft).setOnDragListener(new MyDragListener());
+//        findViewById(R.id.topright).setOnDragListener(new MyDragListener());
+//        findViewById(R.id.bottomleft).setOnDragListener(new MyDragListener());
+//        findViewById(R.id.bottomright).setOnDragListener(new MyDragListener());
     }
 
-    private final class MyTouchListener implements View.OnTouchListener {
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            Log.d("onTouch", String.valueOf(motionEvent.getAction()));
-            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                ClipData data = ClipData.newPlainText("", "");
-                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
-                        view);
-                view.startDrag(data, shadowBuilder, view, 0);
-                view.setVisibility(View.INVISIBLE);
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    class MyDragListener implements View.OnDragListener {
-        Drawable enterShape = getResources().getDrawable(
-                R.drawable.shape_droptarget);
-        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
-
-        @Override
-        public boolean onDrag(View v, DragEvent event) {
-            int action = event.getAction();
-            Log.d("onDrag", String.valueOf(event.getAction()));
-            switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_STARTED:
-                    // do nothing
-                    break;
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    v.setBackgroundDrawable(enterShape);
-                    break;
-                case DragEvent.ACTION_DRAG_EXITED:
-                    v.setBackgroundDrawable(normalShape);
-                    break;
-                case DragEvent.ACTION_DROP:
-                    // Dropped, reassign View to ViewGroup
-                    View view = (View) event.getLocalState();
-                    ViewGroup owner = (ViewGroup) view.getParent();
-                    owner.removeView(view);
-                    LinearLayout container = (LinearLayout) v;
-                    container.addView(view);
-                    view.setVisibility(View.VISIBLE);
-                    break;
-                case DragEvent.ACTION_DRAG_ENDED:
-                    v.setBackgroundDrawable(normalShape);
-                default:
-                    break;
-            }
-            return true;
-        }
-    }
+//    private final class MyTouchListener implements View.OnTouchListener {
+//        public boolean onTouch(View view, MotionEvent motionEvent) {
+//            Log.d("onTouch", String.valueOf(motionEvent.getAction()));
+//            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+//                ClipData data = ClipData.newPlainText("", "");
+//                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+//                        view);
+//                view.startDrag(data, shadowBuilder, view, 0);
+//                view.setVisibility(View.INVISIBLE);
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }
+//    }
+//
+//    class MyDragListener implements View.OnDragListener {
+//        Drawable enterShape = getResources().getDrawable(
+//                R.drawable.shape_droptarget);
+//        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+//
+//        @Override
+//        public boolean onDrag(View v, DragEvent event) {
+//            int action = event.getAction();
+//            Log.d("onDrag", String.valueOf(event.getAction()));
+//            switch (event.getAction()) {
+//                case DragEvent.ACTION_DRAG_STARTED:
+//                    // do nothing
+//                    break;
+//                case DragEvent.ACTION_DRAG_ENTERED:
+//                    v.setBackgroundDrawable(enterShape);
+//                    break;
+//                case DragEvent.ACTION_DRAG_EXITED:
+//                    v.setBackgroundDrawable(normalShape);
+//                    break;
+//                case DragEvent.ACTION_DROP:
+//                    // Dropped, reassign View to ViewGroup
+//                    View view = (View) event.getLocalState();
+//                    ViewGroup owner = (ViewGroup) view.getParent();
+//                    owner.removeView(view);
+//                    LinearLayout container = (LinearLayout) v;
+//                    container.addView(view);
+//                    view.setVisibility(View.VISIBLE);
+//                    break;
+//                case DragEvent.ACTION_DRAG_ENDED:
+//                    v.setBackgroundDrawable(normalShape);
+//                default:
+//                    break;
+//            }
+//            return true;
+//        }
+//    }
 
 }
